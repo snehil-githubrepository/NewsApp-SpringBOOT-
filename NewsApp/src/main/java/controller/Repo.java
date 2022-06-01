47049201c7575ca3a7342127ffe7c53077566cc9
ujjwal sengar <ujsengar@gmail.com>
2022-06-02 00:19:00 +0530

    first commit

---
+++ NewsApp/src/main/java/controller/Repo.java
@@ -0,0 +1,11 @@
+package controller;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Repo extends CrudRepository<Input, Long>{
+
+		
+}
